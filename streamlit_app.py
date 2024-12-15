5f616b77b51666be9fdbd59cb2da552d4c084444
Guillermo M. Roeder Carbo <31332963+mroederc@users.noreply.github.com>
2024-12-15 07:14:28 +0000

    Nuevo titulo y botones

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 GeoCal: Cálculo de propiedades de secciones planas")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider("Selecciona una cantidad: ")
+st.write(f"La cantidad seleccionada es: {cantidad}")
+
+for i in range(cantidad):
+    st.button(f"{i}")
